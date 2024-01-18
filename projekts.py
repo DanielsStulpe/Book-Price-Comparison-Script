4a80909d57d23425de95fef32f17f7e0766f10d0
Daniels Stulpe <Daniels.Stulpe@edu.rtu.lv>
2024-01-18 11:29:00 +0200

    18.01.2024 - 11.28

--- projekts.py
+++ projekts.py
@@ -8,20 +8,24 @@ service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
 
-url = "https://www.janisroze.lv/lv/"
-driver.get(url)
-time.sleep(2)
+books_list = ["Zēns, kuram patika brieži", "Dzeguzēns", "Uz paradīzi"]
 
-book = "Zēns, kuram patika brieži"
+def janisroze_price(books):
+    url = "https://www.janisroze.lv/lv/"
+    driver.get(url)
+    time.sleep(2)
 
-search_input = driver.find_element(By.ID, "search")
-search_input.send_keys(book)
-input_butt = driver.find_element(By.CLASS_NAME, "button")
-input_butt.click()
-s_book = driver.find_element(By.CLASS_NAME, "product-name")
-s_book.click()
+    for book in books:
+        search_input = driver.find_element(By.ID, "search")
+        search_input.clear()
+        search_input.send_keys(book)
+        input_butt = driver.find_element(By.CLASS_NAME, "button")
+        input_butt.click()
+        time.sleep(2)
 
-book_price = driver.find_element(By.CLASS_NAME, "price").text
-print(book_price)
+        book_price = driver.find_element(By.CLASS_NAME, "price").text
+        price =  float(book_price[1::].replace(",", "."))
+        print(price)
 
-input()
+
+janisroze_price(books_list)
